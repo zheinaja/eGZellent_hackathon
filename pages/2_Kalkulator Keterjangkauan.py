55665b262f7e2282a0f4ad6118d50eacf2e81f42
zheinaja <143177162+zheinaja@users.noreply.github.com>
2023-10-21 14:19:43 +0700

    up

---
+++ pages/2_Kalkulator Keterjangkauan.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+"""
+dong dong dong
+"""
+
+import numpy as np
+import streamlit as st
+import pandas as pd
+import folium
+from streamlit_folium import folium_static
+
+#buka data set
+df = pd.read_csv('Data/clean_data_rumah 2023.csv', sep=';')
+umk = pd.read_excel('Data/UMK 2023.xlsx')
+lokasi = pd.read_excel('Data/koordinat Kecamatan Jawa.xlsx')
+lok_kota = pd.read_excel('Data/lonlatkec_kota.xlsx')
+
+
+#buat id kota
+df ['ID_kecamatan'] = df ['ID_kecamatan'].astype (str)
+df['ID_Kota'] =df['ID_kecamatan'].str[:4]
+umk['KODE'] = umk['KODE'].astype (str)
+lokasi.drop(['Unnamed: 4', 'Unnamed: 5', 'location'], axis=1, inplace=True)
+lokasi = lokasi.rename(columns={'lokasi': 'Lokasi'})
+
+
+nama_kota = []
+hntenor_cicilan = []
+suku_bunga = []
+uang_muka = [] 
+gaji = []
+jarak = []
+
+def beli_rumah(nama_kota, hntenor_cicilan, suku_bunga, uang_muka, gaji, jarak):
+  nama_kota = str.upper(nama_kota)
+
+  suku_bunga = suku_bunga/100
+  uang_muka = uang_muka/100
+  max_cicilan = gaji*0.3
+
+
+  #lokasi yang masuk kedalam cicilan (kolom Lokasi)
+  df['cicilan'] = (suku_bunga/12) * (1/ (1- (1+suku_bunga/12)**(-(hntenor_cicilan*12))))* df['Harga'] *(1-uang_muka)
+  df['cicilan'] = df['cicilan'].astype(float)
+  df_beli = df[df['cicilan'] <= max_cicilan]
+
+  #groupby harga di average kolom harga rata2 (Kolom Harga Rata2)
+  df_beli1 = df_beli.groupby(['Lokasi'])['Harga'].mean().reset_index()
+  df_beli1 = round(df_beli1.rename(columns={'Harga': 'Harga Rata2'}))
+
+  #Hitung Jumlah rumah yang dijual (% rumah terbeli)
+  df_beli2 = df_beli.groupby(['Lokasi'])['Harga'].count().reset_index()
+  df_beli2 = df_beli2.rename(columns={'Harga': 'rumah beli'})
+  df_beli3 = df.groupby(['Lokasi'])['Harga'].count().reset_index()
+  df_beli3 = df_beli3.rename(columns={'Harga': 'rumah jual'})
+  df_beli2 = pd.merge(df_beli2, df_beli3, how='left', left_on='Lokasi', right_on='Lokasi')
+  df_beli2['persen rmh terbeli'] = round((df_beli2['rumah beli']/df_beli2['rumah jual'])*100, 2)
+  df_beli1 = pd.merge(df_beli1, df_beli2, how='left', left_on='Lokasi', right_on='Lokasi')
+
+
+  #hitung jarak (kolom jarak terhadap tempat kerja dalam KM)
+  _lat = lok_kota[lok_kota['kota'] == nama_kota]
+  _lat = _lat['latitude']
+  _lat = _lat.iloc[0]
+  _lon = lok_kota[lok_kota['kota'] == nama_kota]
+  _lon = _lon['longitude']
+  _lon = _lon.iloc[0]
+
+  df_beli1 = pd.merge(df_beli1, lokasi, how='left', left_on='Lokasi', right_on='Lokasi')
+
+  def haversine(lat1, lon1, lat2, lon2):
+      R = 6371  # radius of Earth in kilometers
+      phi1 = np.radians(lat1)
+      phi2 = np.radians(lat2)
+      delta_phi = np.radians(lat2 - lat1)
+      delta_lambda = np.radians(lon2 - lon1)
+      a = np.sin(delta_phi/2)**2 + np.cos(phi1) * np.cos(phi2) * np.sin(delta_lambda/2)**2
+      res = R * (2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a)))
+      return np.round(res, 2)
+  
+  df_beli1['jarak km'] = df_beli1.apply(lambda row: haversine(_lat, _lon, row['latitude'], row['longitude']), axis=1)
+  df_beli1 = df_beli1[df_beli1['jarak km'] <= jarak]
+  # df_beli1.drop(['latitude', 'longitude'], axis=1, inplace=True)
+  df_beli1.sort_values(by=['jarak km'], inplace=True)
+  
+  # df_beli1.dropna(subset=['latitude', 'longitude'], inplace=True)
+  
+  #Initialize titik tengah peta. pakai mean dari tiap lokasi di df_beli1
+  
+  mean_lat = df_beli1['latitude'].mean()
+  mean_long = df_beli1['longitude'].mean()
+  
+  map = folium.Map(location=[mean_lat, mean_long], zoom_start=10)
+
+  # Taruh marker di lokasi kerja
+  folium.Marker([_lat, _lon], 
+                popup='Lokasi Kerja',
+                icon=folium.Icon(color='red', icon='cloud')).add_to(map)
+  #Tambah marker untuk lokasi-lokasi rumah potensial
+  for index, row in df_beli1.iterrows():
+    folium.Marker([row['latitude'], row['longitude']], 
+                  popup=f"{row['Lokasi']}").add_to(map)
+
+  df_beli1.drop(['latitude', 'longitude'], axis=1, inplace=True)
+
+  st.dataframe(df_beli1, hide_index=True)
+  # Display the Folium map in Streamlit
+  st.write(map._repr_html_(), unsafe_allow_html=True)
+  
+
+def main():
+   # judul
+   st.title("Lokasi Kerja dan Keterjangkauan Rumah")
+   st.subheader('''
+Berikut penjelasaan daftar input:
+1. Lokasi Kerja - diisi dengan Kabupaten/Kota yang menjadi lokasi kerja masyarakat pencari rumah
+2. Tenor Pinjaman - diisi dengan tahun lamanya KPR dicicl
+3. Suku Bunga -  diisi dengan persentase tingkat suku bunga KPR
+4. Uang Muka - diisi dengan persentase uang muka terhadap harga rumah
+5. Penghasilan per Bulan - diisi dengan penghasilan per bulan dalam rupiah.
+6. Radius Maksimal ke Lokasi Kerja - diisi dengan jarak maksimal rumah ke tempat kerja dalam kilometer.
+untuk menampilkan hasil dapat meng-klik tombol :blue[Lokasi yang dapat terjangkau]
+
+                ''')
+   #input data
+   nama_kota = st.selectbox('Lokasi Kerja',umk['Kabupaten/Kota'])
+   hntenor_cicilan = st.number_input('tenor pinjaman (tahun)', step = 0.1)
+   suku_bunga = st.number_input('suku bunga (%)', step = 0.1)
+   uang_muka = st.number_input('uang muka (%)', step = 0.1)
+   gaji = st.number_input('Penghasilan per Bulan(Rp)', step = 1e6)
+   jarak = st.number_input('Radius Maksimal ke Lokasi Kerja(KM)', step = 0.1)
+
+   terjangkau = ''
+
+
+
+   if st.button('lokasi yang dapat terjangkau'):
+      terjangkau = beli_rumah(nama_kota, hntenor_cicilan, suku_bunga, uang_muka, gaji, jarak)
+
+if __name__ ==  '__main__':
+   main()
+
+#simpan paling bawah ye
+st.sidebar.title('''
+                 Tim eGZellent
+                 1. Zhein Adhi Mahendra Setiawan
+                 2. Gifari Rahmat Alif
+                 ''')
---
+++ pages/3_Inferensi Kausal.py
@@ -0,0 +1,128 @@
+from email.policy import default
+from logging import PlaceHolder
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import statsmodels.formula.api as sm
+from PIL import Image
+
+
+
+
+df = pd.read_csv("Data/clean data rumah 2023 dan tol terdekat.csv", sep = ';')
+hasil_cem = pd.read_csv("Data/hasil cem.csv", sep = ';')
+
+kabkot = df['Kab/Kota']
+kabkot.drop_duplicates(inplace=True)
+jarak_terjauh = df['tol terdekat'].max()
+jarak_terjauh = int(jarak_terjauh)
+
+batas_tol = []
+lokasi = []
+m_jarak = []
+tol = ''
+
+def cem(batas_tol, lokasi, m_jarak):
+    df_run = df.copy()
+    df_run = df_run[df_run['Kab/Kota'].isin(lokasi)]
+    df_run = df_run[df_run['tol terdekat'] <= m_jarak]
+
+    def treatment(row):
+        if row['tol terdekat'] <= batas_tol :
+            val = 1
+        else:
+            val = 0
+        return val
+
+    df_run['tol'] = df_run.apply(treatment, axis=1)
+
+    #log harga
+    df_run['log_harga'] = np.log2(df_run['Harga'])
+
+    # Count how many treated and control observations
+    # are in each bin
+    treated = df_run.loc[df_run['tol'] == 1].groupby(['Luas Tanah','Luas Bangunan', 'Kamar Tidur']).size().to_frame('treated')
+    control = df_run.loc[df_run['tol'] == 0].groupby(['Luas Tanah','Luas Bangunan', 'Kamar Tidur']).size().to_frame('control')
+
+    # Merge those counts back in
+    df_run = df_run.join(treated, on = ['Luas Tanah','Luas Bangunan', 'Kamar Tidur'])
+    df_run = df_run.join(control, on = ['Luas Tanah','Luas Bangunan','Kamar Tidur'])
+
+    # For treated obs, weight is 1 if there are any control matches
+    df_run['weight'] = 0
+    df_run.loc[(df_run['tol'] == 1) & (df_run['control'] > 0), 'weight'] = 1
+
+    # For control obs, weight depends on total number of treated and control
+    # obs that found matches
+    totalcontrols = sum(df_run.loc[df_run['tol']==0]['treated'] > 0)
+    totaltreated = sum(df_run.loc[df_run['tol']==1]['control'] > 0)
+
+    # Then, control weights are treated/control in the bin,
+    # times control/treated overall
+    df_run['controlweights'] = (df_run['treated']/df_run['control'])*(totalcontrols/totaltreated)
+    df_run.loc[(df_run['tol'] == 0), 'weight'] = df_run['controlweights']
+
+    # Now, use the weights to estimate the effect
+    df_run = df_run.replace(np.inf, np.nan).replace(-np.inf, np.nan).dropna()
+
+    result = sm.wls(formula = 'log_harga ~ tol', weights = df_run['controlweights'], data = df_run).fit()
+
+    hasil = pd.read_html(result.summary().tables[1].as_html(),header=0,index_col=0)[0]
+    koef_tol = hasil['coef'].values[1]
+    conf_tol =hasil['P>|t|'].values[1]
+    st.write(f'Dengan tingkat kepercayaan sebesar {100-conf_tol}%, efek keberadaan gerbang tol dengan radius \
+          :blue[{batas_tol} kilometer] dari rumah mengakibatkan peningkatan harga rumah sebesar :red[{np.round(koef_tol*100,2)}%]')
+
+def main():
+    # judul
+    st.title("Efek Rata-Rata Perubahan Harga Rumah diakibatkan Gerbang Tol")
+    #input data
+    st.subheader('''
+                terdapat 2 pilihan analisa inferensi kausal menggunakan Coarsened Exact Matching (CEM), yaitu:
+                1. Average Treatment Effect (ATE)
+                   Menjelaskan dampak rata-rata gerbang tol terhadap harga rumah di Pulau Jawa
+                2. Conditional Average Treatment Effect (CATE)
+                   Melakukan simulasi CATE untuk melihat dampak rata-rata dengan kondisi lokasi Kabupaten/Kota dan jarak tol terhadap rumah
+                ''')
+    opsi_cem = st.selectbox('Pilih salah satu analisa', ['Hasil ATE', 'Simulasi CATE'])
+   
+    if opsi_cem=='Simulasi CATE':
+       lokasi = st.multiselect('Kabupaten/Kota yang disertakan',
+                           kabkot, default = kabkot, placeholder="Pilih Kabupaten/Kota")
+       batas_tol = st.number_input('Jarak rumah ke gerbang Tol terdekat (KM)', step = 0.1,
+                               min_value=0.3)
+       m_jarak = st.slider('Maksimal Jarak Control group', 0, jarak_terjauh , 15)
+       
+       
+       if st.button('Hitung efek gerbang tol terhadap harga rumah'):
+        tol = cem(batas_tol, lokasi,m_jarak)
+    else:
+       st.write('Dalam penghitungan ATE, kita membandingkan treatment grup dan control grup \
+                di Pulau Jawa. treatment yang dipilih adalah efek dari jarak rumah ke gerbang \
+                tol terdekat yang kurang dari sama dengan 2 kilometer. \
+                Tabel berikut adalah ringkasan hasil penghitungan ATE:')
+       
+       d_ate = {' ': ['Outcome', 'Treatment', 'Metode', 'Matching Var', 'Hasil'],
+                'Deskripsi': ['Persentase perubahan harga rumah',
+                         'Rumah dengan jarak ke gerbang tol terdekat kurang 2 kilometer',
+                         'Coarsened Exact Matching (CEM)', 
+                         ' Luas Tanah, Luas Bangunan, dan Jumlah Kamar',
+                         'Terdapat perbedaan harga rumah sebesar 93,55% antara treatment grup dan control grup']} 
+       st.dataframe(d_ate, width=1000, hide_index=True)
+       st.write('Berikut ringkasan statistik hasil perhitungan CEM:')
+       image = Image.open('Data/sum cem.png')
+       st.image(image, caption='Ringkasan Hasil Perhitungan CEM')
+
+
+if __name__ ==  '__main__':
+   main()
+
+#simpan paling bawah ye
+st.sidebar.title('''
+                 Tim eGZellent
+                 1. Zhein Adhi Mahendra Setiawan
+                 2. Gifari Rahmat Alif
+                 ''')
