88380a794b09aa595888bce64cfad12d17303c27
zheinaja <143177162+zheinaja@users.noreply.github.com>
2023-10-23 11:49:15 +0700

    Update 2_Kalkulator.py

--- pages/2_Kalkulator.py
+++ pages/2_Kalkulator.py
@@ -102,7 +102,7 @@ def beli_rumah(nama_kota, hntenor_cicilan, suku_bunga, uang_muka, gaji, jarak):
   
     df_beli1.drop(['latitude', 'longitude'], axis=1, inplace=True)
     df_beli1['Harga Rata2'] = df_beli1['Harga Rata2'].astype(int)
-
+    df_beli1['Harga Rata2'] = df_beli1['Harga Rata2'].apply(lambda x: '{:,}'.format(x))
   
     st.dataframe(df_beli1.style.format({"jarak km": "{:,.2f}", "persen rmh terbeli": "{:,.2f}"})\
       .highlight_min(axis=0), hide_index=True)    # Display the Folium map in Streamlit
